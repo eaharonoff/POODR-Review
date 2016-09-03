1c1496bc5106dec6cd40e2a69a817a0405341767
eaharonoff <emanuel.aharonoff@gmail.com>
2016-09-03 16:43:19 -0400

    classes with single responsibility

--- ch2/gear.rb
+++ ch2/gear.rb
@@ -1,13 +1,36 @@
 class Gear
   attr_reader :chainring, :cog 
   
-  def initialize(chainring, cog)
-    chainring = chainring
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
     @cog = cog 
+    @wheel = Wheel.new(rim,tire)
   end
 
   def ratio
   chainring / cog.to_f
   end
-   
+  #assumes rim and tire sizes are given in inches
+  def gear_inches 
+    ratio * wheel.diameter
+  end
 end
+
+class Wheel
+  attr_reader :rim, :tire
+
+  def intitializer(rim,tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire*2)
+  end 
+
+  def circumference
+    diameter : Math::PI 
+  end
+end
+
+
